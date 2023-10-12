83d1591bbccea58e0b7994199c3e0f46d081133a
joaquin bellora <manuyjoaco123@gmail.com>
2023-10-12 20:12:13 -0300

    Convertidor de Pesos Uruguayos 2.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let savedPass = '1947';
+/* let savedPass = '1947';
 
 function login() {
     let ingresar = false;
@@ -16,10 +16,10 @@ function login() {
         }
         
         return ingresar;
-}
+} */
 
-if (login()) {
-    let montoCambio = 1000;
+/* if (login()) {
+    let montoCambio = prompt('Ingresa la cantidad de  dinero que desees cambiar');
 
     let opcion = prompt('Elige la moneda a la que convertir tus' + montoCambio + 'pesos uruguayos: \n1 - USD. \n2 - Real. \n3 - Euro. \n4 - Finalizar Operacion');
     
@@ -50,4 +50,120 @@ if (login()) {
     }
 } else{
     alert('Se bloqueara su cuenta por seguridad');
+} */
+
+/* const ingresarMonto = parseInt(prompt("Ingresa la cantidad de UYU que desees convertir: "));
+
+const cambiarADolares = () =>{
+    let producto = 0;
+
+    tipoDeCambios.forEach((cambioADolar) => {
+        producto += ingresarMonto*(cambioADolar.valorUnitario)
+    
+    });
+    return producto;
 }
+const cambiarAEuros = () =>{
+    let producto = 0;
+
+    tipoDeCambios.forEach((cambioAEuro) => {
+        producto += ingresarMonto*(cambioAEuro.valorUnitario)
+    
+    });
+    return producto;
+}
+const cambiarAReales = () =>{
+    let producto = 0;
+
+    tipoDeCambios.forEach((cambioAReal) => {
+        producto += ingresarMonto*(cambioAReal.valorUnitario)
+    
+    });
+    return producto;
+}
+
+
+let tipoDeCambios = [
+    {id: '1.', moneda: 'USD', valorUnitario: '38', Funcion: cambiarADolares },
+    {id: '2.', moneda: 'Euro', valorUnitario: '38', Funcion: cambiarAEuros },
+    {id: '3.', moneda: 'Real', valorUnitario: '9', Funcion: cambiarAReales}
+];
+
+
+const opcionesDeCambio = () => {
+    
+    let mensaje = " ";
+    
+    tipoDeCambios.map((tipoDeCambio) => {
+        mensaje += `\n ${tipoDeCambio.id} \n - Moneda: ${tipoDeCambio.moneda} \n - Valor: ${tipoDeCambio.valorUnitario} UYU \n-----------------------------\n`;
+    });  
+    
+    const seleccionarOpcion = prompt(`Elije un Tipo de Cambio: \n ${mensaje} `);
+    
+    const seleccion = seleccionarOpcion.find( opcion => opcion.id === seleccionarOpcion);
+    if (seleccion) {
+        if (seleccion.funcion){
+            const ingresarMonto = parseInt(prompt("Ingresa la cantidad de UYU que desees convertir: "));
+            seleccion.Funcion();
+            }
+
+            } else {
+                alert("Selecciona una opcion correcta. Gracias.")
+                opcionesDeCambio()
+            }
+            
+        };
+
+        opcionesDeCambio();  */
+
+
+
+/* 
+        ---------------------------------------------------------------------------
+        */
+
+        const cambiarADolares = (monto) => {
+            return monto / 38;
+        }
+        
+        const cambiarAEuros = (monto) => {
+            return monto / 38;
+        }
+        
+        const cambiarAReales = (monto) => {
+            return monto / 9;
+        }
+        
+        let tipoDeCambios = [
+            {id: '1', moneda: 'USD', valorUnitario: '38', Funcion: cambiarADolares },
+            {id: '2', moneda: 'Euro', valorUnitario: '38', Funcion: cambiarAEuros },
+            {id: '3', moneda: 'Real', valorUnitario: '9', Funcion: cambiarAReales}
+        ];
+        
+        const opcionesDeCambio = () => {
+            
+            let mensaje = " ";
+            
+            tipoDeCambios.map((tipoDeCambio) => {
+                mensaje += `\n ${tipoDeCambio.id} \n - Moneda: ${tipoDeCambio.moneda} \n - Valor: ${tipoDeCambio.valorUnitario} UYU \n-----------------------------\n`;
+            });  
+            
+            const seleccionarOpcion = prompt(`Elije un Tipo de Cambio: \n ${mensaje} `);
+            
+            const seleccion = tipoDeCambios.find( opcion => opcion.id === seleccionarOpcion);
+            if (seleccion) {
+                if (seleccion.Funcion){
+                    const ingresarMonto = parseInt(prompt("Ingresa la cantidad de UYU que desees convertir: "));
+                    const resultado = seleccion.Funcion(ingresarMonto);
+                    alert(`La cantidad convertida a ${seleccion.moneda} es ${resultado}`);
+                } 
+        
+            } else {
+                alert("Selecciona una opcion correcta. Gracias.")
+                opcionesDeCambio()
+            }
+                    
+        };
+        
+        opcionesDeCambio();
+        
